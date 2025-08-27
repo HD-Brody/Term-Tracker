90ef46fd612b51ebd91c6ae0d2d8b1deefeffd1d
Gr4pejuice <0707bhd@gmail.com>
2025-08-26 21:16:32 -0400

    Create empty calendar page.

---
+++ frontend/src/app/calendar/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Layout from "../../components/Layout";
+
+export default function CalendarPage() {
+  return (
+    <Layout activePage="Calendar">
+      <div className="min-h-screen bg-background">
+      </div>
+    </Layout>
+  );
+}
